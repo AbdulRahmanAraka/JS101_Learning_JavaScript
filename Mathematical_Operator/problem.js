6484608ee9b000537f9bb323f514c83a71c2212d
User1 <ar7028346724@gmail.com>
2022-10-11 08:33:49 +0530

    Mathematical Operator

---
+++ Mathematical_Operator/problem.js
@@ -0,0 +1,4 @@
+let bill = 4000;
+let min_purchase = 3999;
+
+console.log(bill > min_purchase);
---
+++ Mathematical_Operator/problem1.js
@@ -0,0 +1,8 @@
+let x=3
+let mul3;
+    let add10;
+    
+    mul3=x*3;
+    add10=mul3+10;
+    
+    console.log(add10);
